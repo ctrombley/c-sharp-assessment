918b9c1ef131d83304e2b4f05bd4a9e7140aaa4b
Chris Trombley <ctrombley@gmail.com>
2014-10-23 13:14:07 -0700

    more stuff

---
+++ JavaScript1.js
@@ -0,0 +1,18 @@
+﻿(function($) {
+
+	function ex1() {
+
+		function foo(x) {
+			var tmp = 3;
+
+			return function(y) {
+				alert(x + y + (++tmp));
+			};
+		}
+
+		var bar = foo(2);
+		bar(10);
+		bar(10);
+	}
+	
+}(jQuery));
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace InterviewExercises
+{
+	internal class Program
+	{
+		#region Ex1
+
+		private static String location;
+		private static DateTime time;
+
+		// What does the following method write to the console?
+		public static void Ex1()
+		{
+			Console.WriteLine(location == null ? "location is null" : location);
+			Console.WriteLine(time == null ? "time is null" : time.ToString());
+		}
+
+		#endregion
+
+		#region Ex2
+
+		private static string result;
+
+		// What does the following method write to the console?
+		public static void Ex2()
+		{
+			SaySomething();
+			Console.WriteLine(result);
+		}
+
+		private async Task<string> SaySomething()
+		{
+			await Task.Delay(5);
+			result = "Hello world!";
+			return "Something";
+		}
+
+		#endregion
+	}
+}
